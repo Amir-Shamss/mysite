430b30319f4f25efb98480ae9347c1e5bac1cd10
amir_shamsi <amir.shamss21@gmail.com>
2019-08-03 15:01:54 +0430

    the last update

--- web/admin.py
+++ web/admin.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Expense , Income
+from .models import Expense , Income , Token
 
 admin.site.register(Expense)
 admin.site.register(Income)
+admin.site.register(Token)
+
 
